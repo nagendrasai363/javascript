a0966f6d1a22e7d6e06dcec9ca069a68032b74d2
Sai Nagendra <nagendrasai363@gmail.com>
2024-07-30 10:39:49 +0530

    JavaScript Basics

---
+++ JavaScript Basics/identifier_var_6.js
@@ -0,0 +1,7 @@
+var name = "Sai";
+console.log("Welcome to JS Mr." + name);
+
+/* Here, even though we have redeclared the same
+identifier, it will not throw any error.*/
+var name = "Nag";
+console.log("Welcome to JS Mr." + name);
